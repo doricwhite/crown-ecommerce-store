83e6f17ea6886d20392ce51df29ed58eda19246f
Doric <doricwhite@gmail.com>
2022-06-07 11:01:35 -0500

    update categoryMap in firebase utils and write own middleware in store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,24 @@
 import { compose, legacy_createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+// Writing your own Middleware
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
+
+  // Next State
+  next(action);
+  console.log("next state: ", store.getState());
+};
+
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
